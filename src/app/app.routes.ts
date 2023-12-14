b6d9ed811c0ed5bd67536e757a26f6e896e6a45d
luzapo <lumah21@gmail.com>
2023-12-13 21:18:04 -0500

    nueva entrega

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobrenosotrosComponent } from './sobrenosotros/sobrenosotros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "inicio",
+        component: InicioComponent
+    },
+
+    {
+        path: "sobrenosotros",
+        component: SobrenosotrosComponent
+    },
+    {
+        path: "",
+        redirectTo: "/inicio",
+        pathMatch: "full"
+    }
+];
